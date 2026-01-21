459aad9b44f1d47a35032894244368356c479119
conta09 <mucyoconta8@gmail.com>
2026-01-21 21:39:55 +0200

    added place markers on the map/pins

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 
 export default function App() {
@@ -41,21 +41,50 @@ export default function App() {
     );
   }
 
+const places = [
+  { id: 1, lat: 35.113949, lng: 32.850552, name: 'LAU' },
+  { id: 2, lat: 35.111498, lng: 35.111498, name: 'LAU Dorm' },
+];
+
+
   return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        showsUserLocation
-        initialRegion={{
+  <View style={styles.container}>
+    <MapView
+      style={styles.map}
+      showsUserLocation
+      initialRegion={{
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+      }}
+    >
+      {/* User marker 
+      <Marker
+        coordinate={{
           latitude: location.coords.latitude,
           longitude: location.coords.longitude,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
         }}
+        title="You are here"
       />
-    </View>
-  );
-}
+      */}
+
+      {/* Places markers */}
+      {places.map(place => (
+        <Marker
+          
+          key={place.id}
+          coordinate={{
+            latitude: place.lat,
+            longitude: place.lng,
+          }}
+          title={place.name}
+        />
+      ))}
+    </MapView>
+  </View>
+);
+} 
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
